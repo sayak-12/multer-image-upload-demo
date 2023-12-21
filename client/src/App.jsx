404bdb7b50799077e0947c2ee412a6a64d73b962
sayak <Sayakraha12@gmail.com>
2023-12-21 16:40:15 +0530

    added features: multiple images upload, cancelling images, convert into webp and upload

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,29 +1,100 @@
-import { useState } from 'react'
-import axios from 'axios';
-import './App.css'
+import { useState } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
-  const [file, setFile] = useState(null);
-const uploadnow=(e)=>{
-  e.preventDefault();
-  const formData = new FormData();
-  formData.append('file', file);
-  console.log(formData.get('file'));
-  axios.post("http://localhost:3000/upload", formData)
-  .then((response)=>{
-    console.log(response);
-  })
-  .catch((error)=>{console.log(error);})
-}
+  const [files, setFiles] = useState([]);
+
+  const uploadnow = (e) => {
+    e.preventDefault();
+    if (files.length == 0) {
+      alert("Please select at least one image");
+    } else {
+      const formData = new FormData();
+      files.forEach((file) => {
+        formData.append("files", file);
+      });
+
+      axios
+        .post("http://localhost:3000/upload", formData)
+        .then((response) => {
+          console.log(response);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  };
+
+  const handleFileChange = (e) => {
+    const selectedFiles = e.target.files;
+    const imageFiles = [];
+
+    for (let i = 0; i < selectedFiles.length; i++) {
+      const file = selectedFiles[i];
+
+      // Check if the file is an image (you can add more image formats as needed)
+      if (file.type.startsWith("image/")) {
+        imageFiles.push(file);
+      }
+    }
+
+    setFiles(imageFiles);
+  };
+
+  const handleRemoveImage = (index) => {
+    const updatedFiles = [...files];
+    updatedFiles.splice(index, 1);
+    setFiles(updatedFiles);
+  };
+
   return (
-    <>
-      <h1>Uploading images in multer</h1>
-      <form action="/upload" method="post" encType='multipart/form-data'>
-        <input type="file" name="file" id="" onChange={(e)=>{setFile(e.target.files[0])}} /><br />
-        <button type="submit" onClick={uploadnow}>Upload now</button>
-      </form>
-    </>
-  )
+    <form action="/upload" method="post" encType="multipart/form-data">
+      <input
+        type="file"
+        name="files"
+        id=""
+        onChange={handleFileChange}
+        multiple
+        accept="image/*"
+      />
+      <br />
+      {files.length > 0 ? (
+        <div style={{ display: "flex", flexWrap: "wrap", gap: "10px" }}>
+          {files.map((file, index) => (
+            <div key={index} style={{ position: "relative" }}>
+              <div
+                style={{
+                  cursor: "pointer",
+                  position: "absolute",
+                  top: "10px",
+                  right: "10px",
+                  background: "white",
+                  height:"20px",
+                  width:"20px",
+                  borderRadius:"50%"
+                }}
+                className="delete-button"
+                onClick={() => handleRemoveImage(index)}
+              >
+                &#x2715;
+              </div>
+              <img
+                src={URL.createObjectURL(file)}
+                alt={`Preview ${index + 1}`}
+                width={200}
+              />
+            </div>
+          ))}
+        </div>
+      ) : (
+        <span>No image uploaded</span>
+      )}
+      <button type="submit" onClick={uploadnow}>
+        Upload now
+      </button>
+    </form>
+  );
 }
 
-export default App
+export default App;
--- server/app.js
+++ server/app.js
@@ -1,41 +1,65 @@
-const express= require('express');
-const mongoose = require('mongoose');
-const multer = require('multer');
-const cors = require('cors');
-const dotenv = require('dotenv');
-const fs = require('fs');
-const cloud= require('./utils/cloudinary.js');
+const express = require("express");
+const mongoose = require("mongoose");
+const multer = require("multer");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const fs = require("fs");
+const cloud = require("./utils/cloudinary.js");
+const util = require("util");
+const uploadAsync = util.promisify(cloud.uploader.upload);
+const unlinkAsync = util.promisify(fs.unlink);
+const webp = require("webp-converter");
+webp.grant_permission();
 const app = express();
-app.use(express.json())
-app.use(express.urlencoded({ extended:true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, './uploads/')
-    },
-    filename: function (req, file, cb) {
-      
-      cb(null, `${Date.now()}-${file.originalname}`)
-    }
-  })
-  
-  const upload = multer({ storage: storage });
-  app.post('/upload', upload.single('file'),(req, res)=>{
-    cloud.uploader.upload(req.file.path, (err, result)=>{
-        if (err) {
-            console.log(err);
-            return res.status(500).json({
-                output: false
-            })
-        }
-        fs.unlinkSync(req.file.path);
-        res.status(200).json({
-            output:true,
-            result
-        })
-    })
-  });
+  destination: function (req, file, cb) {
+    cb(null, "./uploads/");
+  },
+  filename: function (req, file, cb) {
+    cb(null, `${Date.now()}-${file.originalname}`);
+  },
+});
 
-app.listen(3000, ()=>{
-    console.log("Server listening on port 3000");
-})
+const upload = multer({ storage: storage });
+app.post("/upload", upload.array("files"), async (req, res) => {
+  try {
+    const uploadResults = [];
+
+    for (const file of req.files) {
+      try {
+        const convertedFileName = file.path.replace(/\.[^.]+$/, '.webp');
+        await webp.cwebp(file.path, convertedFileName, "-q 80");
+        const result = await uploadAsync(convertedFileName);
+        await Promise.all([unlinkAsync(file.path), unlinkAsync(convertedFileName)]);
+
+        uploadResults.push(result);
+      } catch (error) {
+        console.error("Error uploading file:", error);
+        return res.status(500).json({
+          output: false,
+          error: "Error uploading file to Cloudinary",
+        });
+      }
+    }
+
+    console.log("All files uploaded successfully:", uploadResults);
+
+    res.status(200).json({
+      output: "ok",
+      uploadResults: uploadResults,
+    });
+  } catch (error) {
+    console.error("Error processing files:", error);
+    res.status(500).json({
+      output: false,
+      error: "Internal Server Error",
+    });
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Server listening on port 3000");
+});
